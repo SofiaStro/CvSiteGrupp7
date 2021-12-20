d7afa222a7ce043a8973284f7d37739fc0590872
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2021-12-20 10:36:54 +0100

    Uppdaterat kolumnerna i message-klassen

--- Data/Models/Message.cs
+++ Data/Models/Message.cs
@@ -12,10 +12,20 @@ namespace Data.Models
     {
         [Key]
         public int Id { get; set; }
-
-        //[ForeignKey("ApplicationUser")]
-        //public int UserID { get; set; }
-        //public virtual ApplicationUser User { get; set; }
+        [Required]
+        [Display(Name = "Avsändare")]
+        public string Sender { get; set; }
+        [Required]
+        [Display(Name = "Datum")]
+        public DateTime SendDate { get; set; }
+        [Required]
+        [Display(Name = "Läst")]
+        public bool Read { get; set; }
+        [Required]
+        [Display(Name = "Meddelande")]
+        public string Content { get; set; }
+        [Required]
+        public string UserName { get; set; }
 
     }
 }

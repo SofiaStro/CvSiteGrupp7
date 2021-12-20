c010a71868ace642635fba099c862da73b9d8a9c
Elin Ahlgren <90694428+ElinAhlgren@users.noreply.github.com>
2021-12-20 11:23:10 +0100

    ProjectRepository

--- CvSiteGrupp7/Controllers/ProjectApiController.cs
+++ CvSiteGrupp7/Controllers/ProjectApiController.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+using RoutePrefixAttribute = System.Web.Http.RoutePrefixAttribute;
 
 namespace CvSiteGrupp7.Controllers
 {
-    public class ProjectApiController : Controller
+    [RoutePrefix("api/project")]
+    public class BookApiController : ApiController
     {
-        // GET: ProjectApi
-        public ActionResult Index()
+        public BookRepository BookRepository
         {
-            return View();
+            get { return new BookRepository(Request.GetOwinContext().Get<BookContext>()); }
+        }
+
+        [HttpPost]   // Anropa med $.ajax eller $.post
+        [Route("delete")] // api/book/delete
+        public IHttpActionResult Delete(ProjectDelete model)
+        {
+            try
+            {
+                var OKDelete = BookRepository.DeleteProject(model.ProjectId);
+                if (OKDelete)
+                {
+                    return Ok();
+                }
+
+                return BadRequest();
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }

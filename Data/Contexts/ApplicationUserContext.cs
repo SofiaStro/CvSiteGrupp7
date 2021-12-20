e87ed5ddbab73ea022efef0a60a2d54956729170
Elin Ahlgren <90694428+ElinAhlgren@users.noreply.github.com>
2021-12-20 16:01:49 +0100

    lagt till alla Dbset i ApplicationDbContext

--- Data/Contexts/ApplicationUserContext.cs
+++ Data/Contexts/ApplicationUserContext.cs
@@ -38,6 +38,10 @@ namespace Data.Contexts
         public DbSet<Project> projects { get; set; }
         public DbSet<Message> messages { get; set; }
         public DbSet<CV> cvs { get; set; }
+        public DbSet<Competence> competences { get; set; }
+        public DbSet<Education> educations { get; set; }
+        public DbSet<Experience> experiences { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();

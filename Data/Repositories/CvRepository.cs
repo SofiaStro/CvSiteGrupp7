2d02fef53d9392339d69a055a8d5620f81045098
Sofia Strömberg <83826297+SofiaStro@users.noreply.github.com>
2021-12-22 09:13:46 +0100

    Skapat klass CvRepository

---
+++ Data/Repositories/CvRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class CvRepository
+    {
+        private readonly CvDBContext _context;
+
+    }
+}

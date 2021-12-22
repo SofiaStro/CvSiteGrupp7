e8336e2a497f8bb36b000b0c06cadce9bcd524ee
Elin Ahlgren <90694428+ElinAhlgren@users.noreply.github.com>
2021-12-22 15:06:10 +0100

    en klass på CVModel i Shared för Sökfunktion

--- Shared/Models/CvModel.cs
+++ Shared/Models/CvModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,13 @@ namespace Shared.Models
         public int Id { get; set; }
         public string CurrentPath { get; set; }
         public HttpPostedFileBase Image { get; set; }
+    }
+
+    public class CvSearch
+    {
+        [Display(Name ="Namn")]
+        public string UserName { get; set; }
+        //public HttpPostedFileBase Image { get; set; }
+
     }
 }

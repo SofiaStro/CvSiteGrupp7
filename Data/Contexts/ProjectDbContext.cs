390247ebda1b9aff798dcaf992413af41f994f70
Elin Ahlgren <90694428+ElinAhlgren@users.noreply.github.com>
2021-12-23 10:27:57 +0100

    lagt till projectDbContext klass i data

---
+++ Data/Contexts/ProjectDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Contexts
+{
+    public class ProjectDbContext : ApplicationDbContext
+    {
+        public ProjectDbContext() : base() { }
+        public DbSet<Project> projects { get; set; }
+    }
+}

bd87d3e034486320f7a3781cc7f48ce8b071fa38
Elin Ahlgren <90694428+ElinAhlgren@users.noreply.github.com>
2021-12-23 09:51:56 +0100

    fungerande sökfunktion för CV

--- CvSiteGrupp7/Controllers/SearchController.cs
+++ CvSiteGrupp7/Controllers/SearchController.cs
@@ -11,10 +11,15 @@ namespace CvSiteGrupp7.Controllers
     {
         private CvDBContext db = new CvDBContext();
         // GET: Search
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
             //var cvs = db.cvs.Where(row => row.Private == false).ToList();
-            var cvs = db.cvs.ToList();
+            var cvs = from c in db.cvs select c;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                cvs = cvs.Where(s => s.Name.Contains(searchString));
+            }
+            //var cvs = db.cvs.ToList();
             return View(cvs);
         }
 

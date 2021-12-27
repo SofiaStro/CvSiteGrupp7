d816aeee0dbbb37fc334973ffa18b442c53b6602
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2021-12-27 17:02:54 +0100

    lagt till OWIN paket i nuget

--- CvSiteGrupp7/Controllers/MessageApiController.cs
+++ CvSiteGrupp7/Controllers/MessageApiController.cs
@@ -1,11 +1,13 @@
 ﻿using Data.Contexts;
 using Data.Models;
 using Data.Repositories;
+using Microsoft.AspNet.Identity.Owin;
 using Services;
 using Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -14,7 +16,11 @@ namespace CvSiteGrupp7.Controllers
     [RoutePrefix("api/message")]
     public class MessageApiController : ApiController
     {
-        MessageRepository messageRepository = new MessageRepository();
+        //MessageRepository messageRepository = new MessageRepository();
+        public MessageRepository messageRepository
+        {
+            get { return new MessageRepository(Request.GetOwinContext().Get<MessageDbContext>()); }
+        }
         //MessageService messageService = new MessageService();
 
         ////Använda för att sätta siffran till notiser??

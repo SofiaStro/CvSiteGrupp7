f18942d94be04f0803976ff060be1af18c8378eb
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2021-12-28 14:19:34 +0100

    städat kod och ändrat lite namn

--- Shared/Models/MessageModel.cs
+++ Shared/Models/MessageModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,10 @@ namespace Shared.Models
 {
     public class MessageModel
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
+        [Display(Name = "Avsändare")]
         public string Sender { get; set; }
+        [Display(Name = "Meddelande")]
         public string Content { get; set; }
         public bool Read { get; set; }
         public string UserName{ get; set; }

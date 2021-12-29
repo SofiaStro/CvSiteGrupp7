186a9940a585f189bc5b94044a4d4106eb618605
Elin Ahlgren <90694428+ElinAhlgren@users.noreply.github.com>
2021-12-29 14:02:23 +0100

    lagt till [required] i projectModel, Shared

--- Shared/Models/ProjectModel.cs
+++ Shared/Models/ProjectModel.cs
@@ -5,10 +5,13 @@ namespace Shared.Models
 {
     public class ProjectCreateModel
     {
+        [Required]
         [Display(Name = "Projektnamn")]
         public string Name { get; set; }
+        [Required]
         [Display(Name = "Beskrivning")]
         public string Description { get; set; }
+        [Required]
         [Display(Name = "Datum")]
         [DataType(DataType.Date)]
         public DateTime AddedDate { get; set; }

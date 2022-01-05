45cb25d31d140c24d787ba1bc6681eedc9f01799
Moa Hedlund <83755862+moahedlund@users.noreply.github.com>
2022-01-05 14:56:44 +0100

    Kontrollerat alla Views

--- CvSiteGrupp7/Startup.cs
+++ CvSiteGrupp7/Startup.cs
@@ -11,11 +11,6 @@ namespace CvSiteGrupp7
         {
             ConfigureAuth(app);
 
-            /*
-           * För Controllers (vanliga): behöver ha NuGet för Microsoft.Owin.Host.SystemWeb (installeras med template för Identity) för att kunna hämta via HttpContext.GetOwinContext()
-           * För ApiControllers: behöver ha NuGet för Microsoft.AspNet.WebApi.Owin för att kunna hämta via Request.GetOwinContext()
-           * För att hämta contextet via Get metoden så behöver man använda namespacet: Microsoft.AspNet.Identity.Owin ex. GetOwinContext().Get<BookContext>()
-           */
             app.CreatePerOwinContext(() => new MessageDbContext());
         }
     }
